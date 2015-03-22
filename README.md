ee4c4e0ae4fde708d2a18d89c53f1dc0b509657b
Mauro Porras P <mauro.porrasp@gmail.com>
2015-03-21 23:50:18 -0500

    Update scripts

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Mauro Porras P dotvim
 
-`./bootstrap.sh`
+````
+cd
+rm -rf ~/.vim ~/.vimrc
+git clone git@github.com:mporras/dotvim.git ~/.vim
+~/.vim/bootstrap.sh
+````
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env sh
 
+echo 'Installing Pathogen.'
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+  curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+echo 'Creating symbolic link.'
 ln -s ~/.vim/vimrc ~/.vimrc
+
+echo 'Installing plugins.'
+cd ~/.vim
+git submodule init
+git submodule update
