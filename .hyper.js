b0efc582d167ec558c7ec403ca770825c0ea201d
mauroporras <mauroporrasc@gmail.com>
2019-06-08 13:38:42 -0500

    chore: Remove Solarized

--- .hyper.js
+++ .hyper.js
@@ -63,11 +63,6 @@ module.exports = {
     modifierKeys: {
       altIsMeta: true
     },
-
-    hyper_solarized: {
-      background: 'light',
-      cursorColor: 'magenta',
-    },
   },
 
   // a list of plugins to fetch and install from npm
@@ -76,7 +71,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyper-solarized"],
+  plugins: [],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
