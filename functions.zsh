f6b58354ebdfa5bc1a729ce3c3eb12c717136819
Mauro Porras <mauroporrasc@gmail.com>
2019-12-04 12:15:15 -0500

    feat: Update zsh plugins

--- functions.zsh
+++ functions.zsh
@@ -1,6 +1,6 @@
 eval "$(direnv hook zsh)"
 
 # Misc.
-plugins=(bundler docker httpie rails wd yarn)
+plugins=(colored-man-pages colorize docker httpie wd zsh-completions)
 bindkey "^P" up-line-or-search
 bindkey "^N" down-line-or-search
