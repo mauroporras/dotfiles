3c5f5dac709aba71d066504801d58750af543c2b
mauroporras <167700+mauroporras@users.noreply.github.com>
2022-03-27 18:08:04 -0500

    remove gutenye/json5.vim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -36,7 +36,6 @@ return require('packer').startup(function(use)
   use 'antonk52/vim-browserslist'
   use 'editorconfig/editorconfig-vim'
   use 'euclidianAce/BetterLua.vim'
-  use 'gutenye/json5.vim'
   use 'honza/vim-snippets'
   use 'jiangmiao/auto-pairs'
   use 'justinmk/vim-sneak'
