274fb9ef1049297fbfbce11bbea9d04f2fe56b22
mauroporras <167700+mauroporras@users.noreply.github.com>
2022-03-27 18:32:47 -0500

    WIP: native-lsp

--- lua/plugins-settings.lua
+++ lua/plugins-settings.lua
@@ -28,12 +28,14 @@ require'lspconfig'.yamlls.setup{}
 require('nvim-treesitter.configs').setup({
   -- one of: "all", "maintained" (parsers with maintainers), or a list of languages.
   ensure_installed = "maintained",
+  -- Install languages synchronously (only applied to `ensure_installed`).
+  sync_install = false,
   -- List of parsers to ignore installing.
   ignore_install = { },
   highlight = {
-    -- false will disable the whole extension
+    -- false will disable the whole extension.
     enable = true,
-    -- list of language that will be disabled
+    -- list of languages that will be disabled.
     disable = { },
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
