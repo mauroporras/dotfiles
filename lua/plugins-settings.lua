816b7ff65d6d3a5271ade1ab7f8ada9e8f69023c
mauroporras <167700+mauroporras@users.noreply.github.com>
2022-11-27 11:56:55 -0500

    chore: update for nvim-cmp

--- lua/plugins-settings.lua
+++ lua/plugins-settings.lua
@@ -31,7 +31,7 @@ vim.g.neoformat_try_node_exe = 1
 -- {{{ nvim-cmp.
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 local lspconfig = require('lspconfig')
 
