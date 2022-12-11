a389a6a10439ffc9906b0f578b1b12fa96c51340
mauroporras <167700+mauroporras@users.noreply.github.com>
2022-12-11 13:48:55 -0500

    feat(Vim): move colors-italics.lua
    Into init.lua

--- init.lua
+++ init.lua
@@ -372,7 +372,57 @@ vim.api.nvim_set_keymap('t', '<M-r>', '<C-\\><C-n>:RnvimrResize<CR>', optNRM)
 --   vim-which-key.
 vim.api.nvim_set_keymap('n', '<Leader>', ":WhichKey '<Space>'<CR>", optNRM)
 
-require('colors-italics')
+-- To show the current scheme:
+--   :colorscheme
+-- Use `:highlight` to list all color groups.
+--   :h :highlight
+
+-- Enable 24-bit RGB color.
+-- See:
+-- https://neovim.io/doc/user/options.html#'termguicolors'
+vim.opt.termguicolors = true
+
+vim.opt.background = 'light'
+
+vim.cmd([[
+  " See:
+  " https://github.com/rakr/vim-one#italic-support
+  let g:one_allow_italics = 1
+
+  colorscheme one
+
+  autocmd TextYankPost * silent! lua vim.highlight.on_yank({ higroup = "HighlightedyankRegion", timeout = 400 })
+
+  " Custom highlights.
+  highlight HighlightedyankRegion guibg=violet guifg=black
+
+  highlight LeapLabelPrimary guibg=cyan guifg=black
+
+  highlight Comment gui=italic
+
+  highlight StatusLine guibg=black guifg=white
+  highlight StatusLineNC guibg=black guifg=lightgray
+
+  highlight TabLine guibg=lightgray guifg=black
+  highlight TabLineFill guibg=lightgray guifg=black
+  highlight TabLineSel guibg=black guifg=white
+
+  highlight VertSplit guibg=white guifg=black
+
+  highlight IncSearch guibg=cyan guifg=black
+  highlight Search guibg=cyan guifg=black
+  highlight Substitute guibg=cyan guifg=black
+
+  highlight clear MatchParen
+  highlight MatchParen guibg=black guifg=magenta
+
+  highlight QuickFixLine guibg=lime guifg=black
+
+  highlight QuickScopePrimary guibg=cyan guifg=black
+  highlight QuickScopeSecondary guibg=black guifg=cyan
+
+  highlight CursorLine guibg=lightcyan
+]])
 
 -- Searching.
 vim.opt.ignorecase = true
