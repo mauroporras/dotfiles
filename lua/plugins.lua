fc6064f8233eee94037fbc68faa24485758fb665
mauroporras <167700+mauroporras@users.noreply.github.com>
2022-12-11 13:18:21 -0500

    feat(Vim): update Packer bootstrapping

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -9,16 +9,19 @@ vim.cmd([[
 -- Install "packer.nvim" if not present.
 -- See:
 -- https://github.com/wbthomason/packer.nvim#bootstrapping
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-
-  packer_bootstrap = fn.system({
-    'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path
-  })
-
-  print('Packer has been installed into "'..install_path..'". Restart Neovim and run `:PackerSync`.')
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    print('Packer has been installed into "'..install_path..'". Restart Neovim and run `:PackerSync`.')
+    return true
+  end
+  return false
 end
+
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
   -- Packer can manage itself.
