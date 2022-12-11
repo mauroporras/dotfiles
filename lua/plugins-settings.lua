1e080172f350b43be40d95140a659a435992b29d
mauroporras <167700+mauroporras@users.noreply.github.com>
2022-12-11 12:36:38 -0500

    feat(Vim): add lspconfig for "sumneko_lua"

--- lua/plugins-settings.lua
+++ lua/plugins-settings.lua
@@ -18,6 +18,28 @@ require'lspconfig'.diagnosticls.setup{}
 require'lspconfig'.dockerls.setup{}
 require'lspconfig'.html.setup{}
 require'lspconfig'.jsonls.setup{}
+require'lspconfig'.sumneko_lua.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 require'lspconfig'.rust_analyzer.setup{}
 require'lspconfig'.stylelint_lsp.setup{}
 require'lspconfig'.svelte.setup{}
