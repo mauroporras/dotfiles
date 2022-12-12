aa41e7727da1038f27090b5587cae386d20e3425
mauroporras <167700+mauroporras@users.noreply.github.com>
2022-12-11 19:35:46 -0500

    feat(Vim): improve mappings for nvim-cmp

--- init.lua
+++ init.lua
@@ -125,8 +125,7 @@ vim.g.neoformat_try_node_exe = 1
 
 -- {{{ nvim-cmp.
 -- Add additional capabilities supported by nvim-cmp
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local lspconfig = require('lspconfig')
 
@@ -154,7 +153,7 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- Server-specific settings:
+-- Put plugins with server-specific settings here:
 require'lspconfig'.sumneko_lua.setup {
   on_attach = on_attach,
   capabilities = capabilities,
@@ -181,22 +180,42 @@ require'lspconfig'.sumneko_lua.setup {
 }
 
 -- luasnip setup
-require('luasnip')
+local luasnip = require('luasnip')
 
 -- nvim-cmp setup
 local cmp = require 'cmp'
 cmp.setup {
   snippet = {
     expand = function(args)
-      require('luasnip').lsp_expand(args.body)
+      luasnip.lsp_expand(args.body)
     end,
   },
   mapping = cmp.mapping.preset.insert({
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
   }),
   sources = {
     { name = 'nvim_lsp' },
