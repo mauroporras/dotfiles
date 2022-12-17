fe56a0f6d593c99b35531cf3cff8de5a9c431d3d
mauroporras <167700+mauroporras@users.noreply.github.com>
2022-12-17 17:20:07 -0500

    feat(Vim): add Telescope mappings similar to fzf

--- init.lua
+++ init.lua
@@ -261,6 +261,12 @@ require('telescope').setup {
       "--no-heading",
       "--color=never",
     },
+    mappings = {
+      i = {
+        ["<C-k>"] = require('telescope.actions').move_selection_previous,
+        ["<C-j>"] = require('telescope.actions').move_selection_next,
+      },
+    },
   },
   pickers = {
     -- :h telescope.builtin.find_files
