c0708f4dc8b1f11e6ea03de7b7a61dcf7d00d172
mauroporras <167700+mauroporras@users.noreply.github.com>
2024-07-19 15:12:47 -0500

    WIP: experimenting

--- functions.zsh
+++ functions.zsh
@@ -1,6 +1,6 @@
 eval "$(direnv hook zsh)"
 
 # Misc.
-plugins=(colored-man-pages colorize docker httpie wd zsh-completions)
+plugins=(colored-man-pages colorize docker httpie zsh-completions)
 bindkey "^P" up-line-or-search
 bindkey "^N" down-line-or-search
