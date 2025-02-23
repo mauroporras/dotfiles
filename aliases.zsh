aa6f92e80bb5b2cf9047c6e76a7d742914a17d17
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-02-23 12:54:38 -0500

    chore: organize aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,4 +1,9 @@
+alias e='nvim'
 alias f='yazi'
+alias g='git'
+alias l='ls -lAhF'
+alias t='task'
+alias v='lazygit'
 
 alias rg="rg --follow --hidden --smart-case --no-ignore --glob '!{.git,dist,node_modules,tags}'"
 
@@ -27,17 +32,9 @@ alias dkmu='docker compose up'
 alias dkmub='docker compose up --build'
 # }}}
 
-# Git {{{
-alias g='git'
-alias gt='github' # GitHub CLI.
-alias lg='lazygit'
-# }}}
-
 # Jobs {{{
 alias j='jobs'
 alias k1='kill %1'
 alias k2='kill %2'
 alias k3='kill %3'
 # }}}
-
-alias l='ls -lAhF'
