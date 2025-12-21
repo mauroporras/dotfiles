6d9557e12d6aa817a6a5351621761bdd69e4c4d1
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-20 22:07:29 -0500

    feat: add example configs for new projects

---
+++ example-configs/shell.production.nix
@@ -0,0 +1,20 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    cowsay
+    go
+    go-task
+    lolcat
+    wget
+  ];
+
+  GREETING = "Using Production";
+
+  shellHook = ''
+    echo $GREETING | cowsay | lolcat
+  '';
+}
