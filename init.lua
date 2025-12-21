c34d091e30c2609106fe0ca02857fc2d837de738
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-21 15:03:06 -0500

    WIP: feat/lazy-nvim-migration

--- init.lua
+++ init.lua
@@ -555,13 +555,6 @@ require('lazy').setup {
       --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
 
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
-
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
