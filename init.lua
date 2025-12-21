a14da88188611efbbffefcec48beed94123ff23e
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-21 09:05:14 -0500

    feat(Neovim): add easier terminal mode exit with Esc Esc

--- init.lua
+++ init.lua
@@ -108,6 +108,14 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>cq', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
 -- Misc.
 vim.opt.completeopt = {
   'menu',
