f123943346cf34eb2db1b9baa242e47e66f80230
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-21 10:58:18 -0500

    refactor(Neovim): comment out packer plugins during lazy.nvim migration

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,35 +1,4 @@
--- This file is external to init.lua so that it doesn't
--- trigger `PackerSync` every time it changes.
-
--- Runs whenever this file is updated.
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]]
-
--- Install "packer.nvim" if not present.
--- See:
--- https://github.com/wbthomason/packer.nvim#bootstrapping
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
-    vim.cmd [[packadd packer.nvim]]
-    print('Packer has been installed into "' .. install_path .. '". Restart Neovim and run `:PackerSync`.')
-    return true
-  end
-  return false
-end
-
-local packer_bootstrap = ensure_packer()
-
-return require('packer').startup(function(use)
-  -- Packer can manage itself.
-  use 'wbthomason/packer.nvim'
-
+--[[
   -- LSP and friends.
   -- See:
   -- https://github.com/neovim/nvim-lspconfig/wiki/Autocompletion
@@ -246,10 +215,4 @@ return require('packer').startup(function(use)
   }
 
   use 'github/copilot.vim'
-
-  -- Automatically set up your configuration after cloning packer.nvim.
-  -- Put this at the end after all plugins.
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+--]]
