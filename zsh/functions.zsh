4866b7383e78ff47502c40f02cd3061bbb348b4e
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-20 22:15:54 -0500

    feat(zsh): add zsh-completions with case-insensitive matching

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -2,7 +2,8 @@
 # Rembember to check from time to time if the
 # way to load the utilities has changed.
 
-# https://github.com/junegunn/fzf#setting-up-shell-integration
+# fzf
+# See: https://github.com/junegunn/fzf#setting-up-shell-integration
 source <(fzf --zsh)
 
 # https://direnv.net/docs/hook.html#zsh
@@ -13,6 +14,21 @@ eval "$(zoxide init zsh)"
 
 # https://starship.rs/guide/#step-2-set-up-your-shell-to-use-starship
 eval "$(starship init zsh)"
+
+# zsh-completions
+# See: https://github.com/zsh-users/zsh-completions
+#
+# If you're getting security warnings, run:
+#   brew info zsh-completions
+# and follow the instructions.
+if type brew &>/dev/null; then
+  FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+
+  zstyle ':completion:*' matcher-list 'm:{[:lower:]}={[:upper:]}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+  autoload -Uz compinit
+  compinit
+fi
 
 # Custom bindings
 bindkey "^p" up-line-or-search
