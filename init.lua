ef294fcdfd1c05c20645341b000fbe0171d620df
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-21 09:06:08 -0500

    docs(Neovim): add commented tip to disable arrow keys

--- init.lua
+++ init.lua
@@ -116,6 +116,12 @@ vim.keymap.set('n', '<leader>cq', vim.diagnostic.setloclist, { desc = 'Open diag
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+-- TIP: Disable arrow keys in normal mode
+-- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
 -- Misc.
 vim.opt.completeopt = {
   'menu',
