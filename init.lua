9c7e1288f16eb8b235945a9c60e16892adc12aca
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-21 08:42:55 -0500

    feat(Neovim): add scrolloff for cursor context

--- init.lua
+++ init.lua
@@ -90,6 +90,9 @@ vim.o.inccommand = 'split'
 -- Show which line your cursor is on
 vim.o.cursorline = true
 
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 10
+
 -- Misc.
 vim.opt.completeopt = {
   'menu',
