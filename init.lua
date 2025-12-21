b83660434b97f073ef448f9a884dc47a200928dd
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-20 22:35:01 -0500

    refactor(Neovim): simplify trouble.nvim config to defaults

--- init.lua
+++ init.lua
@@ -228,20 +228,7 @@ vim.g.user_emmet_mode = 'i'
 require('gitsigns').setup()
 
 -- folke/trouble.nvim
-require("trouble").setup {
-  icons = false,
-  fold_open = "v", -- icon used for open folds
-  fold_closed = ">", -- icon used for closed folds
-  indent_lines = false, -- add an indent guide below the fold icons
-  signs = {
-    -- icons / text used for a diagnostic
-    error = "error",
-    information = "info",
-    hint = "hint",
-    warning = "warn",
-  },
-  use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
-}
+require("trouble").setup()
 
 -- lualine {{{
 local custom_lualine_theme = require('lualine.themes.codedark')
