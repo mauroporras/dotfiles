317d65e26bdfb3466cfb3df7e0e88a3e78768284
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-21 08:01:56 -0500

    feat(Neovim): add have_nerd_font global variable

--- init.lua
+++ init.lua
@@ -10,6 +10,9 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
 require 'plugins'
 require 'snippets'
 
