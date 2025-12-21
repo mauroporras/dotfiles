bea95ba27f8093f01e883dfa89438837f9d8622c
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-21 09:07:16 -0500

    refactor(Neovim): move window navigation keymaps to main config

--- init.lua
+++ init.lua
@@ -122,6 +122,21 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
+-- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
+-- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
+-- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
+-- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+
 -- Misc.
 vim.opt.completeopt = {
   'menu',
@@ -539,11 +554,6 @@ require('legendary').setup {
     { '<Leader>tp', ':-tabnew<CR>', description = 'Opens tabpage before the current.' },
     { '<M-h>', ':tabprevious<CR>', description = 'Previous tab.' },
     { '<M-l>', ':tabnext<CR>', description = 'Nex tab.' },
-    -- Windows.
-    { '<C-h>', '<C-w>h', description = 'Window left.' },
-    { '<C-l>', '<C-w>l', description = 'Window right.' },
-    { '<C-j>', '<C-w>j', description = 'Window up.' },
-    { '<C-k>', '<C-w>k', description = 'Window down.' },
     -- Windows. Temporal.
     { '<C-w><C-w>', '', description = 'Do nothing. To unlearn shortcut.' },
     { '<C-w>h', '', description = 'Do nothing. To unlearn shortcut.' },
