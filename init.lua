02ddac0782465ff98b822bc9690ed418d89985d9
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-21 13:22:22 -0500

    WIP: feat/lazy-nvim-migration

--- init.lua
+++ init.lua
@@ -578,9 +578,9 @@ require('lazy').setup({
     'mikavilpas/yazi.nvim',
     dependencies = { 'folke/snacks.nvim' },
     keys = {
-      { '<Leader>f', '<cmd>Yazi<CR>', desc = 'Open file manager focused on the current file.' },
-      { '<Leader>Fc', '<cmd>Yazi cwd<CR>', desc = 'Open file manager focused on the `cwd`.' },
-      { '<Leader>Fl', '<cmd>Yazi toggle<CR>', desc = 'Open file manager focused on the `cwd`.' },
+      { '<Leader>f', '<cmd>Yazi<CR>', desc = 'Open file manager focused on the current file' },
+      { '<Leader>Fc', '<cmd>Yazi cwd<CR>', desc = 'Open file manager focused on the `cwd`' },
+      { '<Leader>Fl', '<cmd>Yazi toggle<CR>', desc = 'Open file manager focused on the `cwd`' },
     },
     opts = {},
   },
