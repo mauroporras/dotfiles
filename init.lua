d4c77520c0080a9046e1097c65e1ba99afe811f1
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-21 14:32:37 -0500

    WIP: feat/lazy-nvim-migration

--- init.lua
+++ init.lua
@@ -236,6 +236,8 @@ rtp:prepend(lazypath)
 --    :Lazy update
 --
 -- NOTE: Here is where you install your plugins.
+
+-- Plugins {{{
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
@@ -771,6 +773,7 @@ require('lazy').setup({
     },
   },
 })
+-- }}}
 
 -- Misc.
 vim.opt.completeopt = {
