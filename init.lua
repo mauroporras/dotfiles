553f0ca085f219855edcd67a96c25284772471d5
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-21 15:09:36 -0500

    WIP: feat/lazy-nvim-migration

--- init.lua
+++ init.lua
@@ -340,10 +340,26 @@ require('lazy').setup {
     event = 'VimEnter',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      'nvim-telescope/telescope-frecency.nvim',
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-      'nvim-telescope/telescope-smart-history.nvim',
+      { -- If encountering errors, see telescope-fzf-native README for installation instructions
+        'nvim-telescope/telescope-fzf-native.nvim',
+
+        -- `build` is used to run some command when the plugin is installed/updated.
+        -- This is only run then, not every time Neovim starts up.
+        build = 'make',
+
+        -- `cond` is a condition used to determine whether this plugin should be
+        -- installed and loaded.
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
       'nvim-telescope/telescope-ui-select.nvim',
+
+      -- Useful for getting pretty icons, but requires a Nerd Font.
+      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+
+      'nvim-telescope/telescope-frecency.nvim',
+      'nvim-telescope/telescope-smart-history.nvim',
       'kkharji/sqlite.lua', -- Required by frecency and smart_history
     },
     keys = {
