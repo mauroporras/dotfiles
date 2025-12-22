3d486e84680c53dab95b77542b1080330c6d477b
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-22 15:28:37 -0500

    WIP: feat/add-lazy-nvim

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -3,4 +3,9 @@ return {
   dependencies = {
     "folke/snacks.nvim",
   },
+  keys = {
+    { "<Leader>f", "<cmd>Yazi<CR>", desc = "Open file manager focused on the current file" },
+    { "<Leader>Fc", "<cmd>Yazi cwd<CR>", desc = "Open file manager focused on the cwd" },
+    { "<Leader>Fl", "<cmd>Yazi toggle<CR>", desc = "Resume last yazi session" },
+  },
 }
