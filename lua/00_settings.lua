d15df69bdebb6328a6aaf750253e1fdd2aef7a21
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-23 18:37:31 -0500

    WIP: feat/add-lazy-nvim

--- lua/00_settings.lua
+++ lua/00_settings.lua
@@ -30,9 +30,9 @@ vim.o.showmode = false
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.o.clipboard = "unnamedplus"
-end)
+-- vim.schedule(function()
+--   vim.o.clipboard = "unnamedplus"
+-- end)
 
 -- Enable break indent
 vim.o.breakindent = true
