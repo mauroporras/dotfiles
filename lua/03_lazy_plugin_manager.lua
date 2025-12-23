c5fa8c53707886552d8ef086225f2efe3a857359
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-23 10:18:18 -0500

    WIP: feat/add-lazy-nvim

--- lua/03_lazy_plugin_manager.lua
+++ lua/03_lazy_plugin_manager.lua
@@ -1,6 +1,15 @@
 -- vim:foldmethod=marker
 
--- Bootstrap lazy.nvim
+--  See:
+--    `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  You can press `?` in this menu for help. Use `:q` to close the window
+--
+--  To update plugins you can run
+--    :Lazy update
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
