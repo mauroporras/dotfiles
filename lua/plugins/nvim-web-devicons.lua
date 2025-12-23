d57b6d659f0128ea52133cde0860c18142a7357a
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-23 10:46:09 -0500

    WIP: feat/add-lazy-nvim

--- lua/plugins/nvim-web-devicons.lua
+++ lua/plugins/nvim-web-devicons.lua
@@ -1,3 +1,4 @@
 return {
   "nvim-tree/nvim-web-devicons",
+  lazy = true,
 }
--- lua/plugins/quick-scope.lua
+++ lua/plugins/quick-scope.lua
@@ -1,6 +1,7 @@
 -- Easy left and right motion.
 return {
   "unblevable/quick-scope",
+  event = "VeryLazy",
   init = function()
     vim.g.qs_highlight_on_keys = { "f", "F", "t", "T" }
   end,
