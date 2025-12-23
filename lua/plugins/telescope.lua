ecbf6496b829ff2fe6f9631e83d1cd62b4c5cd89
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-23 12:12:12 -0500

    WIP: feat/add-lazy-nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -41,7 +41,7 @@ return {
     { "<Leader>O", "<cmd>Telescope frecency workspace=CWD<CR>", desc = "Frecency file picker" },
     { "<Leader>o", "<cmd>Telescope find_files<CR>", desc = "Find files" },
     { "<Leader>vc", "<cmd>Telescope git_bcommits<CR>", desc = "Buffer git commits" },
-    { "<Leader>zl", "<cmd>Telescope current_buffer_fuzzy_find<CR>", desc = "Fuzzy find in buffer" },
+    { "<Leader>/", "<cmd>Telescope current_buffer_fuzzy_find<CR>", desc = "Fuzzy find in buffer" },
     { "<Leader>zh", "<cmd>Telescope oldfiles<CR>", desc = "Recent files" },
     { "<Leader>zr", "<cmd>Telescope resume<CR>", desc = "Resume last picker" },
   },
