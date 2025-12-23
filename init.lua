fc40147d15f315f5a088da5ac3bb06ddf78ce17a
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-23 09:52:16 -0500

    WIP: feat/add-lazy-nvim

--- init.lua
+++ init.lua
@@ -15,17 +15,6 @@ require("03_lazy_plugin_manager")
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.hl.on_yank()`
-vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-  callback = function()
-    vim.hl.on_yank({ higroup = "HighlightedyankRegion" })
-  end,
-})
-
 local optNRM = { noremap = true }
 
 --[[
--- lua/00_settings.lua
+++ lua/00_settings.lua
@@ -106,3 +106,14 @@ vim.cmd([=[
   " Recognize some extensions known to have JSON with comments.
   autocmd BufNewFile,BufRead tsconfig.json setlocal filetype=jsonc
 ]=])
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.hl.on_yank({ higroup = "HighlightedyankRegion" })
+  end,
+})
