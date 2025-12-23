41cb180fc75be496a6985d0a8530a93cf886ab22
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-23 09:38:17 -0500

    WIP: feat/add-lazy-nvim

---
+++ lua/03_highlights.lua
@@ -0,0 +1,28 @@
+vim.cmd([[
+  " Custom highlights.
+  highlight HighlightedyankRegion guibg=violet guifg=black
+
+  highlight IlluminatedWordText guibg=paleturquoise
+  highlight IlluminatedWordRead guibg=paleturquoise
+  highlight IlluminatedWordWrite guibg=paleturquoise
+
+  highlight IblIndent guifg=lavender
+  highlight IblScope guifg=lavender
+
+  highlight StatusLine guibg=black guifg=white
+  highlight StatusLineNC guibg=gray guifg=lightgray
+
+  highlight TabLine guibg=lightgray guifg=black
+  highlight TabLineFill guibg=lightgray guifg=black
+  highlight TabLineSel guibg=black guifg=white
+
+  highlight VertSplit guifg=gray
+
+  highlight CurSearch guibg=black guifg=cyan
+  highlight IncSearch guibg=cyan guifg=black
+  highlight Search guibg=cyan guifg=black
+  highlight Substitute guibg=cyan guifg=black
+
+  highlight QuickScopePrimary guibg=cyan guifg=black
+  highlight QuickScopeSecondary guibg=black guifg=cyan
+]])
