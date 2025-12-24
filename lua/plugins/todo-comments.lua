ee58a6ce56c422a675e1230850fbc67375dcb74b
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-24 10:18:25 -0500

    WIP: feat/add-lazy-nvim

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,27 +1,44 @@
 -- Highlights in comments:
+--
 -- FIX: Something that needs to be fixed
+-- FIXME: Something that needs to be fixed
+-- BUG: A bug that needs to be resolved
+-- FIXIT: Something that needs to be fixed
+-- ISSUE: An issue that needs attention
+--
 -- TODO: Something that needs to be done
+--
 -- HACK: A workaround for a problem
+--
 -- WARN: Something that is potentially problematic
+-- WARNING: Something that is potentially problematic
+-- XXX: Something that is potentially problematic
+--
 -- PERF: A code section that could be optimized for better performance
+-- OPTIM: A code section that could be optimized
+-- PERFORMANCE: A code section that could be optimized for better performance
+-- OPTIMIZE: A code section that could be optimized
+--
 -- NOTE: Something important to note
+-- INFO: Informational comment
+--
 -- TEST: A section of code that is used for testing
+-- TESTING: A section of code that is used for testing
+-- PASSED: A test that has passed
+-- FAILED: A test that has failed
+--
+-- NOTE: To see all the keywords and their settings, run the following command in Neovim:
+-- :lua print(vim.inspect(require("todo-comments.config").options.keywords
 return {
   "folke/todo-comments.nvim",
   event = "VeryLazy",
   -- Lua utility library used by many plugins
   dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
-    { "<Leader>ma", "<Cmd>TodoTelescope<CR>", desc = "All to-do marks" },
-    { "<Leader>mf", "<Cmd>TodoTelescope keywords=FIX<CR>", desc = "FIX to-do marks" },
-    { "<Leader>mt", "<Cmd>TodoTelescope keywords=TODO<CR>", desc = "TODO to-do marks" },
-    { "<Leader>mh", "<Cmd>TodoTelescope keywords=HACK<CR>", desc = "HACK to-do marks" },
-    { "<Leader>mw", "<Cmd>TodoTelescope keywords=WARN<CR>", desc = "WARN to-do marks" },
-    { "<Leader>mp", "<Cmd>TodoTelescope keywords=PERF<CR>", desc = "PERF to-do marks" },
-    { "<Leader>mn", "<Cmd>TodoTelescope keywords=NOTE<CR>", desc = "NOTE to-do marks" },
-    { "<Leader>ms", "<Cmd>TodoTelescope keywords=TEST<CR>", desc = "TEST to-do marks" },
-    { "<Leader>mi", "<Cmd>TodoTelescope keywords=FIX,TODO<CR>", desc = "Important to-do marks" },
+    { "<Leader>ma", "<Cmd>TodoTelescope<CR>", desc = "Telescope all to-do marks" },
     { "<Leader>mq", "<Cmd>TodoQuickFix<CR>", desc = "QuickFix all to-do marks" },
+    { "<Leader>mf", "<Cmd>TodoTelescope keywords=FIX,FIXME,BUG,FIXIT,ISSUE<CR>", desc = "Telescope FIX to-do marks" },
+    { "<Leader>mt", "<Cmd>TodoTelescope keywords=TODO<CR>", desc = "Telescope TODO to-do marks" },
   },
   opts = {
     signs = false,
