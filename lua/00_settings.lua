c8b55f58cd8becd9458d4b479383ee479d2f6354
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-24 07:38:07 -0500

    WIP: feat/add-lazy-nvim

--- lua/00_settings.lua
+++ lua/00_settings.lua
@@ -1,19 +1,17 @@
 -- vim:foldmethod=marker
 
+-- NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 -- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+-- See `:h mapleader`
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- [[ Setting options ]]
--- See `:help vim.o`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
+-- See `:h vim.o`
+--  For more options, you can see `:h option-list`
 
 -- Make line numbers default
 vim.o.number = true
--- You can also add relative line numbers, to help with jumping.
+-- You can also add relative line numbers, to h with jumping.
 --  Experiment for yourself to see if you like it!
 -- vim.o.relativenumber = true
 
@@ -26,7 +24,7 @@ vim.o.showmode = false
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
+--  See `:h 'clipboard'`
 -- vim.schedule(function()
 --   vim.o.clipboard = "unnamedplus"
 -- end)
@@ -55,13 +53,13 @@ vim.o.splitright = true
 vim.o.splitbelow = true
 
 -- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
+--  See `:h 'list'`
+--  and `:h 'listchars'`
 --
 --  Notice listchars is set using `vim.opt` instead of `vim.o`.
 --  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
---   See `:help lua-options`
---   and `:help lua-options-guide`
+--   See `:h lua-options`
+--   and `:h lua-options-guide`
 vim.o.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 --[[
@@ -87,7 +85,7 @@ vim.o.scrolloff = 10
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
+-- See `:h 'confirm'`
 vim.o.confirm = true
 
 -- Misc.
@@ -104,11 +102,11 @@ vim.cmd([=[
 ]=])
 
 -- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
+--  See `:h lua-guide-autocommands`
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
---  See `:help vim.hl.on_yank()`
+--  See `:h vim.hl.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
   group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
