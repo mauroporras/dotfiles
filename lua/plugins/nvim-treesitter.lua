39111d8bff85a8aca995e1c6db9a3a27c6e3870d
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-24 13:48:54 -0500

    WIP: feat/add-lazy-nvim

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,59 +2,122 @@
 -- Highlight, edit, and navigate code
 -- See: `:h lsp-vs-treesitter`
 return {
-  "nvim-treesitter/nvim-treesitter",
-  lazy = false, -- NOTE: this plugin does not support lazy-loading.
-  build = ":TSUpdate",
-  ---@alias lazyvim.TSConfig: TSConfig
-  opts = {
-    ensure_installed = {
-      "bash",
-      "c",
-      "cmake",
-      "cpp",
-      "css",
-      "diff",
-      "dockerfile",
-      "git_rebase",
-      "gitattributes",
-      "gitcommit",
-      "gitignore",
-      "go",
-      "gomod",
-      "gosum",
-      "gowork",
-      "html",
-      "javascript",
-      "jsdoc",
-      "json",
-      "json5",
-      "jsonc",
-      "lua",
-      "luadoc",
-      "luap",
-      "make",
-      "markdown",
-      "markdown_inline",
-      "printf",
-      "python",
-      "query",
-      "regex",
-      "scss",
-      "svelte",
-      "toml",
-      "tsx",
-      "typescript",
-      "vim",
-      "vimdoc",
-      "xml",
-      "yaml",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    lazy = false, -- NOTE: this plugin does not support lazy-loading.
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "c",
+        "cmake",
+        -- "cpp", -- Takes too long
+        "css",
+        "diff",
+        "dockerfile",
+        "git_rebase",
+        "gitattributes",
+        "gitcommit",
+        "gitignore",
+        "go",
+        "gomod",
+        "gosum",
+        "gowork",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "json5",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "printf",
+        "python",
+        "query",
+        "regex",
+        "scss",
+        "svelte",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
+      },
     },
-  },
-  config = function(_, opts)
-    local TS = require("nvim-treesitter")
+    config = function(_, opts)
+      -- install parsers from custom opts.ensure_installed
+      if opts.ensure_installed and #opts.ensure_installed > 0 then
+        require("nvim-treesitter").install(opts.ensure_installed)
+        -- register and start parsers for filetypes
+        for _, parser in ipairs(opts.ensure_installed) do
+          local filetypes = parser -- In this case, parser is the filetype/language name
+          vim.treesitter.language.register(parser, filetypes)
 
-    TS.setup(opts)
-  end,
+          vim.api.nvim_create_autocmd({ "FileType" }, {
+            pattern = filetypes,
+            callback = function(event)
+              vim.treesitter.start(event.buf, parser)
+            end,
+          })
+        end
+      end
+
+      -- Auto-install and start parsers for any buffer
+      vim.api.nvim_create_autocmd({ "BufRead" }, {
+        callback = function(event)
+          local bufnr = event.buf
+          local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
+
+          -- Skip if no filetype
+          if filetype == "" then
+            return
+          end
+
+          -- Check if this filetype is already handled by explicit opts.ensure_installed config
+          for _, filetypes in pairs(opts.ensure_installed) do
+            local ft_table = type(filetypes) == "table" and filetypes or { filetypes }
+            if vim.tbl_contains(ft_table, filetype) then
+              return -- Already handled above
+            end
+          end
+
+          -- Get parser name based on filetype
+          local parser_name = vim.treesitter.language.get_lang(filetype) -- might return filetype (not helpful)
+          if not parser_name then
+            return
+          end
+          -- Try to get existing parser (helpful check if filetype was returned above)
+          local parser_configs = require("nvim-treesitter.parsers")
+          if not parser_configs[parser_name] then
+            return -- Parser not available, skip silently
+          end
+
+          local parser_installed = pcall(vim.treesitter.get_parser, bufnr, parser_name)
+
+          if not parser_installed then
+            -- If not installed, install parser asynchronously
+            require("nvim-treesitter").install({ parser_name })
+            return -- Parser will be available on next buffer open
+          end
+
+          -- let's check again
+          parser_installed = pcall(vim.treesitter.get_parser, bufnr, parser_name)
+
+          if parser_installed then
+            -- Start treesitter for this buffer
+            vim.treesitter.start(bufnr, parser_name)
+          end
+        end,
+      })
+    end,
+  },
 }
 
 -- WARN: Don't delete this, kept for reference.
@@ -75,7 +138,7 @@ ensure_installed = {
   "bash",
   "c",
   "cmake",
-  "cpp",
+  -- "cpp", -- Takes too long
   "css",
   "diff",
   "dockerfile",
