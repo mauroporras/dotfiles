4e4a880265565f7a187c5e5d308347391e9dc00a
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-24 07:46:12 -0500

    WIP: feat/add-lazy-nvim

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,7 +1,7 @@
 -- Highlight todo, notes, etc in comments
 return {
   "folke/todo-comments.nvim",
-  event = "BufReadPost",
+  event = "VeryLazy",
   -- Lua utility library used by many plugins
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {
