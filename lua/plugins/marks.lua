2e2ae909d586c31090af207843e1a3286b57bb26
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-24 07:17:30 -0500

    WIP: feat/add-lazy-nvim

--- lua/plugins/marks.lua
+++ lua/plugins/marks.lua
@@ -1,11 +1,17 @@
 -- Show Vim marks in the sign column.
 -- See:
 --    :h mark
+--
+-- 'a (single quote):
+--   Jump to the beginning of the line where mark "a" was set.
+-- `a (backtick):
+--   Jump to the exact cursor position (row and column) where mark "a" was set.
 return {
   "chentoast/marks.nvim",
   event = "BufReadPost",
   opts = {
-    default_mappings = false,
+    -- Disable all default keymaps
+    -- default_mappings = false,
     refresh_interval = 333,
   },
 }
