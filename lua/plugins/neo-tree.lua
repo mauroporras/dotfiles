0995250b37152f693c898ef59d7cc75a164e4ac7
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-24 08:40:50 -0500

    WIP: feat/add-lazy-nvim

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,45 @@
+-- File explorer sidebar.
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    keys = {
+      { "<Leader>zt", "<Cmd>Neotree toggle<CR>", desc = "Toggle file explorer" },
+      { "<Leader>zb", "<Cmd>Neotree buffers<CR>", desc = "Open buffers in explorer" },
+    },
+  },
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-neo-tree/neo-tree.nvim", -- makes sure that this loads after Neo-tree.
+    },
+    config = function()
+      require("lsp-file-operations").setup()
+    end,
+  },
+  {
+    "s1n7ax/nvim-window-picker",
+    version = "2.*",
+    config = function()
+      require("window-picker").setup({
+        filter_rules = {
+          include_current_win = false,
+          autoselect_one = true,
+          -- filter using buffer options
+          bo = {
+            -- if the file type is one of following, the window will be ignored
+            filetype = { "neo-tree", "neo-tree-popup", "notify" },
+            -- if the buffer type is one of following, the window will be ignored
+            buftype = { "terminal", "quickfix" },
+          },
+        },
+      })
+    end,
+  },
+}
