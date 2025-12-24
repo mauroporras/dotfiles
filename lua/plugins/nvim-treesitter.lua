86aedc1806162b31cc08a8dacf26fe390c22a484
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-24 14:09:46 -0500

    WIP: feat/add-lazy-nvim

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -5,116 +5,80 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     branch = "master",
-    lazy = false, -- NOTE: this plugin does not support lazy-loading.
+    lazy = false,
     build = ":TSUpdate",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "c",
-        "cmake",
-        -- "cpp", -- Takes too long
-        "css",
-        "diff",
-        "dockerfile",
-        "git_rebase",
-        "gitattributes",
-        "gitcommit",
-        "gitignore",
-        "go",
-        "gomod",
-        "gosum",
-        "gowork",
-        "html",
-        "javascript",
-        "jsdoc",
-        "json",
-        "json5",
-        "jsonc",
-        "lua",
-        "luadoc",
-        "luap",
-        "make",
-        "markdown",
-        "markdown_inline",
-        "printf",
-        "python",
-        "query",
-        "regex",
-        "scss",
-        "svelte",
-        "toml",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "xml",
-        "yaml",
-      },
-    },
-    config = function(_, opts)
-      -- install parsers from custom opts.ensure_installed
-      if opts.ensure_installed and #opts.ensure_installed > 0 then
-        require("nvim-treesitter").install(opts.ensure_installed)
-        -- register and start parsers for filetypes
-        for _, parser in ipairs(opts.ensure_installed) do
-          local filetypes = parser -- In this case, parser is the filetype/language name
-          vim.treesitter.language.register(parser, filetypes)
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        -- A list of parser names, or "all"
+        ensure_installed = {
+          "bash",
+          "c",
+          "cmake",
+          "cpp",
+          "css",
+          "diff",
+          "dockerfile",
+          "git_rebase",
+          "gitattributes",
+          "gitcommit",
+          "gitignore",
+          "go",
+          "gomod",
+          "gosum",
+          "gowork",
+          "html",
+          "javascript",
+          "jsdoc",
+          "json",
+          "json5",
+          "jsonc",
+          "lua",
+          "luadoc",
+          "luap",
+          "make",
+          "markdown",
+          "markdown_inline",
+          "printf",
+          "python",
+          "query",
+          "regex",
+          "scss",
+          "svelte",
+          "toml",
+          "tsx",
+          "typescript",
+          "vim",
+          "vimdoc",
+          "xml",
+          "yaml",
+        },
 
-          vim.api.nvim_create_autocmd({ "FileType" }, {
-            pattern = filetypes,
-            callback = function(event)
-              vim.treesitter.start(event.buf, parser)
-            end,
-          })
-        end
-      end
+        -- Install parsers synchronously (only applied to `ensure_installed`)
+        sync_install = false,
 
-      -- Auto-install and start parsers for any buffer
-      vim.api.nvim_create_autocmd({ "BufRead" }, {
-        callback = function(event)
-          local bufnr = event.buf
-          local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
+        -- Automatically install missing parsers when entering buffer
+        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+        auto_install = true,
 
-          -- Skip if no filetype
-          if filetype == "" then
-            return
-          end
+        -- List of parsers to ignore installing (for "all")
+        ignore_install = {},
 
-          -- Check if this filetype is already handled by explicit opts.ensure_installed config
-          for _, filetypes in pairs(opts.ensure_installed) do
-            local ft_table = type(filetypes) == "table" and filetypes or { filetypes }
-            if vim.tbl_contains(ft_table, filetype) then
-              return -- Already handled above
-            end
-          end
+        highlight = {
+          -- `false` will disable the whole extension
+          enable = true,
 
-          -- Get parser name based on filetype
-          local parser_name = vim.treesitter.language.get_lang(filetype) -- might return filetype (not helpful)
-          if not parser_name then
-            return
-          end
-          -- Try to get existing parser (helpful check if filetype was returned above)
-          local parser_configs = require("nvim-treesitter.parsers")
-          if not parser_configs[parser_name] then
-            return -- Parser not available, skip silently
-          end
+          -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+          -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+          -- the name of the parser)
+          -- list of language that will be disabled
+          disable = {},
 
-          local parser_installed = pcall(vim.treesitter.get_parser, bufnr, parser_name)
-
-          if not parser_installed then
-            -- If not installed, install parser asynchronously
-            require("nvim-treesitter").install({ parser_name })
-            return -- Parser will be available on next buffer open
-          end
-
-          -- let's check again
-          parser_installed = pcall(vim.treesitter.get_parser, bufnr, parser_name)
-
-          if parser_installed then
-            -- Start treesitter for this buffer
-            vim.treesitter.start(bufnr, parser_name)
-          end
-        end,
+          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+          -- Using this option may slow down your editor, and you may see some duplicate highlights.
+          -- Instead of true it can also be a list of languages
+          additional_vim_regex_highlighting = false,
+        },
       })
     end,
   },
