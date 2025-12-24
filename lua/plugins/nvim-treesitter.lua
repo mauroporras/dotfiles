6efb265dd4fe540345327d6dc92e0aaa34c1ca69
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-24 12:44:59 -0500

    WIP: feat/add-lazy-nvim

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -6,15 +6,17 @@ return {
   lazy = false, -- NOTE: this plugin does not support lazy-loading.
   build = ":TSUpdate",
   -- See `:h nvim-treesitter`
-  opts = {
-    -- A list of parser names, or "all"
-    --   See:
-    --     https://github.com/nvim-treesitter/nvim-treesitter/blob/main/SUPPORTED_LANGUAGES.md
-    --   To update all parsers:
-    --     :TSUpdate
-    --   To list all available commands:
-    --     :h nvim-treesitter-commands
-    ensure_installed = {
+  -- A list of parser names, or "all"
+  --   See:
+  --     https://github.com/nvim-treesitter/nvim-treesitter/blob/main/SUPPORTED_LANGUAGES.md
+  --   To update all parsers:
+  --     :TSUpdate
+  -- List installed: :TSInstallInfo
+  --   To list all available commands:
+  --     :h nvim-treesitter-commands
+  config = function()
+    -- Install parsers (no-op if already installed)
+    require("nvim-treesitter").install({
       "bash",
       "c",
       "cmake",
@@ -50,16 +52,20 @@ return {
       "vim",
       "vimdoc",
       "yaml",
-    },
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { "ruby" },
-    },
-    indent = { enable = true, disable = { "ruby" } },
-  },
+    })
+
+    -- Enable treesitter highlighting for all filetypes
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
+    })
+
+    -- Enable treesitter-based indentation (experimental)
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
 }
