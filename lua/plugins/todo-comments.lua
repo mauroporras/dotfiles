191de66545eee20abfe418f124b60cac788e67c4
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-24 08:25:17 -0500

    WIP: feat/add-lazy-nvim

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -21,6 +21,8 @@ return {
     { "<Leader>mn", "<Cmd>TodoTelescope keywords=NOTE<CR>", desc = "NOTE to-do marks" },
     { "<Leader>ms", "<Cmd>TodoTelescope keywords=TEST<CR>", desc = "TEST to-do marks" },
     { "<Leader>mq", "<Cmd>TodoQuickFix<CR>", desc = "QuickFix all to-do marks" },
+    { "<Leader>mi", "<Cmd>TodoTelescope keywords=FIX,TODO<CR>", desc = "Important to-do marks" },
+    { "<Leader>mq", "<Cmd>TodoQuickFix<CR>", desc = "QuickFix all to-do marks" },
   },
   opts = {
     signs = false,
