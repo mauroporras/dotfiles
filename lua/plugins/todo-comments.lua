a8be9e36fcf738a53e959e6414d9b6de3ef11500
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-24 07:49:16 -0500

    WIP: feat/add-lazy-nvim

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,4 +1,11 @@
--- Highlight todo, notes, etc in comments
+-- Highlights in comments:
+-- FIX: Something that needs to be fixed
+-- TODO: Something that needs to be done
+-- HACK: A workaround for a problem
+-- WARN: Something that is potentially problematic
+-- PERF: A code section that could be optimized for better performance
+-- NOTE: Something important to note
+-- TEST: A section of code that is used for testing
 return {
   "folke/todo-comments.nvim",
   event = "VeryLazy",
