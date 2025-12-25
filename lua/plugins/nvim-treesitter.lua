bfa9810f795fab54c91ed5d198cc729ab5597629
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-25 13:31:04 -0500

    WIP: feat/add-lazy-nvim

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,6 +3,10 @@
 -- See: `:h lsp-vs-treesitter`
 return {
   "nvim-treesitter/nvim-treesitter",
+  -- WARN:This plugin does not support lazy-loading.
   lazy = false,
   build = ":TSUpdate",
+  config = function(_, opts)
+    require("nvim-treesitter").install({})
+  end,
 }
