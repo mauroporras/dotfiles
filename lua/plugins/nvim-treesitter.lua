c2fcca6521ec7841302524d4f2fa151580b793c8
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-25 13:47:07 -0500

    WIP: feat/add-lazy-nvim

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,13 +1,62 @@
 -- Treesitter provides syntax highlighting and code parsing.
--- Highlight, edit, and navigate code
--- See: `:h lsp-vs-treesitter`
+--
+-- See `:h lsp-vs-treesitter`
 -- Run `:checkhealth nvim-treesitter` if troubles.
+--
+-- See `:h nvim-treesitter`
 return {
   "nvim-treesitter/nvim-treesitter",
   -- WARN:This plugin does not support lazy-loading.
   lazy = false,
   build = ":TSUpdate",
   config = function(_, opts)
-    require("nvim-treesitter").install({})
+    -- A list of parser names
+    --   See:
+    --     https://github.com/nvim-treesitter/nvim-treesitter/blob/main/SUPPORTED_LANGUAGES.md
+    --   Update all parsers `:TSUpdate`
+    --   List installed `:checkhealth nvim-treesitter`
+    --   List all available commands `:h nvim-treesitter-commands`
+    require("nvim-treesitter").install({
+      "bash",
+      "c",
+      "cmake",
+      "cpp",
+      "css",
+      "diff",
+      "dockerfile",
+      "git_rebase",
+      "gitattributes",
+      "gitcommit",
+      "gitignore",
+      "go",
+      "gomod",
+      "gosum",
+      "gowork",
+      "html",
+      "javascript",
+      "jsdoc",
+      "json",
+      "json5",
+      "jsonc",
+      "lua",
+      "luadoc",
+      "luap",
+      "make",
+      "markdown",
+      "markdown_inline",
+      "printf",
+      "python",
+      "query",
+      "regex",
+      "scss",
+      "svelte",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "xml",
+      "yaml",
+    })
   end,
 }
