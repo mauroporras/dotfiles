339364c213d6893b4a14b5ac14cf55f41b54de8d
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-25 15:28:22 -0500

    WIP: feat/add-lazy-nvim

---
+++ lua/plugins/pretty-fold.lua
@@ -0,0 +1,11 @@
+-- Prettier fold text display.
+-- https://github.com/anuvyklack/pretty-fold.nvim
+-- TODO: Configure pretty-fold.nvim
+--
+--[[
+return {
+  "anuvyklack/pretty-fold.nvim",
+  event = "BufReadPost",
+  opts = {},
+}
+--]]
