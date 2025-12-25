e71ee554c7433e2ddc46300d973444d719602ede
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-25 13:38:09 -0500

    WIP: feat/add-lazy-nvim

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,7 @@
 -- Treesitter provides syntax highlighting and code parsing.
 -- Highlight, edit, and navigate code
 -- See: `:h lsp-vs-treesitter`
+-- Run `:checkhealth nvim-treesitter` if troubles.
 return {
   "nvim-treesitter/nvim-treesitter",
   -- WARN:This plugin does not support lazy-loading.
