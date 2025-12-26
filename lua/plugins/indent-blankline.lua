ff75e0359786cc769e0a30e2855c1857e3075cea
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-26 13:03:49 -0500

    WIP: feat/add-lazy-nvim

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -4,6 +4,8 @@ return {
   "lukas-reineke/indent-blankline.nvim",
   event = "BufReadPost",
   main = "ibl",
+  ---@module "ibl"
+  ---@type ibl.config
   opts = {
     indent = {
       char = "│",
