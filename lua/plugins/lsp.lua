f1b45e0efbd6120ae156572e7bfb0c9edb055d94
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-26 07:46:43 -0500

    WIP: feat/add-lazy-nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -137,7 +137,13 @@ return {
           source = "if_many",
           spacing = 2,
           format = function(diagnostic)
-            return diagnostic.message
+            local diagnostic_message = {
+              [vim.diagnostic.severity.ERROR] = diagnostic.message,
+              [vim.diagnostic.severity.WARN] = diagnostic.message,
+              [vim.diagnostic.severity.INFO] = diagnostic.message,
+              [vim.diagnostic.severity.HINT] = diagnostic.message,
+            }
+            return diagnostic_message[diagnostic.severity]
           end,
         },
       })
@@ -258,24 +264,42 @@ return {
     ---@type blink.cmp.Config
     opts = {
       keymap = {
-        -- 'default' for mappings similar to built-in completions
-        --   <c-y> to accept the completion
-        --   <c-space>: Open menu or open docs if already open
-        --   <c-n>/<c-p> or <up>/<down>: Select next/previous item
-        --   <c-e>: Hide menu
-        --   <c-k>: Toggle signature help
+        -- 'default' (recommended) for mappings similar to built-in completions
+        --   <c-y> to accept ([y]es) the completion.
+        --    This will auto-import if your LSP supports it.
+        --    This will expand snippets if the LSP sent a snippet.
+        -- 'super-tab' for tab to accept
+        -- 'enter' for enter to accept
+        -- 'none' for no mappings
+        --
+        -- For an understanding of why the 'default' preset is recommended,
+        -- you will need to read `:help ins-completion`
+        --
+        -- No, but seriously. Please read `:help ins-completion`, it is really good!
+        --
+        -- All presets have the following mappings:
+        -- <tab>/<s-tab>: move to right/left of your snippet expansion
+        -- <c-space>: Open menu or open docs if already open
+        -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
+        -- <c-e>: Hide menu
+        -- <c-k>: Toggle signature help
+        --
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = "default",
+
+        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       },
 
       appearance = {
-        -- 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
         nerd_font_variant = "mono",
       },
 
       completion = {
-        -- Press `<c-space>` to show the documentation.
-        -- Set `auto_show = true` to show the documentation after a delay.
+        -- By default, you may press `<c-space>` to show the documentation.
+        -- Optionally, set `auto_show = true` to show the documentation after a delay.
         documentation = { auto_show = false, auto_show_delay_ms = 500 },
       },
 
@@ -288,7 +312,13 @@ return {
 
       snippets = { preset = "luasnip" },
 
-      -- Use Lua fuzzy matcher (set to 'prefer_rust_with_warning' for rust implementation)
+      -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
+      -- which automatically downloads a prebuilt binary when enabled.
+      --
+      -- By default, we use the Lua implementation instead, but you may enable
+      -- the rust implementation via `'prefer_rust_with_warning'`
+      --
+      -- See :h blink-cmp-config-fuzzy for more information
       fuzzy = { implementation = "lua" },
 
       -- Shows a signature help window while you type arguments for a function
