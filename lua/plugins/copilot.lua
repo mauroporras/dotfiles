2e26a597ae90860e86b522f88ef6fcc0fe1bd1c9
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-26 12:20:35 -0500

    WIP: feat/add-lazy-nvim

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,7 +2,7 @@
 -- GitHub AI code completion.
 return {
   "github/copilot.vim",
-  event = "InsertEnter",
+  event = "BufReadPost",
   -- See `:h copilot-maps`
   keys = {
     { "<C-l>", "<Plug>(copilot-accept-word)", mode = "i", desc = "Accept Copilot word" },
