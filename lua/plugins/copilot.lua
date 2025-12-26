a998506ddd71c8811c3c1bab9f6efbdd363d4110
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-26 11:18:04 -0500

    Revert "WIP: feat/add-lazy-nvim"
    This reverts commit 029634b38595e347d5066a07d8bd368ebdf75c73.

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,12 +1,9 @@
+-- TODO: check official docs
 -- GitHub AI code completion.
 return {
   "github/copilot.vim",
   event = "InsertEnter",
-  init = function()
-    vim.g.copilot_no_tab_map = true
-  end,
   keys = {
     { "<C-l>", "<Plug>(copilot-accept-word)", mode = "i", desc = "Accept Copilot word" },
-    { "<C-j>", "<Plug>(copilot-accept-line)", mode = "i", desc = "Accept Copilot suggestion" },
   },
 }
