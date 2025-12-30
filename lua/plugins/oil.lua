696da688ade38e95b91434fefdab8f8ec069be1c
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-30 08:44:47 -0500

    WIP: feat/add-lazy-nvim

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,9 +1,12 @@
--- TODO: check official docs
 -- File explorer that lets you edit the filesystem like a buffer.
 return {
   "stevearc/oil.nvim",
   event = "VeryLazy",
+  ---@module 'oil'
+  ---@type oil.SetupOpts
   opts = {
+    skip_confirm_for_simple_edits = true,
+    delete_to_trash = true,
     view_options = {
       show_hidden = true,
     },
