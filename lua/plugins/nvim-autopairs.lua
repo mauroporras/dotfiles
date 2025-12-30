c52381b7d3ae78fd78e661cae5487afffab827f8
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-29 23:48:17 -0500

    WIP: feat/add-lazy-nvim

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -1,10 +1,11 @@
--- TODO: check official docs
 -- Auto-close brackets, quotes, and other pairs.
 return {
   "windwp/nvim-autopairs",
   event = "InsertEnter",
   opts = {
+    -- Map the <C-h> key to delete a pair.
     map_c_h = true,
+    -- Map <c-w> to delete a pair if possible.
     map_c_w = true,
   },
 }
