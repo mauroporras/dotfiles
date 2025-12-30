8e56959646751deaa4a29d306d73b09949b25651
mauroporras <167700+mauroporras@users.noreply.github.com>
2025-12-30 00:05:22 -0500

    WIP: feat/add-lazy-nvim

--- lua/plugins/nvim-navbuddy.lua
+++ lua/plugins/nvim-navbuddy.lua
@@ -1,9 +1,9 @@
--- TODO: check official docs
 -- Breadcrumb navigation popup for LSP symbols.
 return {
   "SmiteshP/nvim-navbuddy",
   cmd = "Navbuddy",
   dependencies = {
+    "SmiteshP/nvim-navic",
     "MunifTanjim/nui.nvim",
   },
   opts = {
