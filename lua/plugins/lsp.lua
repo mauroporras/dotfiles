2966d61c5489d01eabf73634c94f1d91f54bef7d
mauroporras <167700+mauroporras@users.noreply.github.com>
2026-03-01 22:09:02 -0500

    fix(lsp): use rust fuzzy matcher and fix LuaSnip version prefix

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -315,7 +315,7 @@ return {
       -- Snippet Engine
       {
         "L3MON4D3/LuaSnip",
-        version = "2.*",
+        version = "v2.*",
         build = (function()
           -- Build Step is needed for regex support in snippets.
           -- This step is not supported in many windows environments.
@@ -402,14 +402,15 @@ return {
 
       snippets = { preset = "luasnip" },
 
-      -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
+      -- blink.cmp includes an optional, recommended rust fuzzy matcher,
       -- which automatically downloads a prebuilt binary when enabled.
       --
       -- By default, we use the Lua implementation instead, but you may enable
       -- the rust implementation via `'prefer_rust_with_warning'`
       --
       -- See :h blink-cmp-config-fuzzy for more information
-      fuzzy = { implementation = "lua" },
+      -- fuzzy = { implementation = "lua" },
+      fuzzy = { implementation = "prefer_rust_with_warning" },
 
       -- Shows a signature help window while you type arguments for a function
       signature = { enabled = true },
