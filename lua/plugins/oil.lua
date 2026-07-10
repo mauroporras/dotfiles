c407e2e0940cd9e54057b3be781ff0c406f00274
mauroporras <167700+mauroporras@users.noreply.github.com>
2026-07-10 18:18:04 -0500

    config(oil): disable window-navigation keybinds

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -7,6 +7,12 @@ return {
   opts = {
     skip_confirm_for_simple_edits = true,
     delete_to_trash = true,
+    -- Free up <C-h>/<C-l> (oil binds them by default) so they fall through
+    -- to the global window-navigation mappings.
+    keymaps = {
+      ["<C-h>"] = false,
+      ["<C-l>"] = false,
+    },
     view_options = {
       show_hidden = true,
     },
